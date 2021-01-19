57b3fda49fee13e2239b3dcdaf81ed84ea039742
Matthew Easley <matteasley8791@gmail.com>
2021-01-19 16:36:34 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -25,8 +25,8 @@ end
 
 def sorted_matrix
     sorted_matrix = []
-      CONVENTIONAL_PRODUCE.sort!
-      ORGANIC_PRODUCE.sort!
+     sorted_matrix << CONVENTIONAL_PRODUCE.sort
+      ORGANIC_PRODUCE.sort
       sorted_matrix = [CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE]
       sorted_matrix
 end
